b373b8507f85434800d10df76dd9a6d41e2823c7
lyndj <lyndel.joyce@gmail.com>
2018-03-27 04:47:23 +0000

    update databaseurl

--- app.js
+++ app.js
@@ -16,8 +16,10 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index")
 
-//mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://lyndj:horsedata@ds123919.mlab.com:23919/yelpcampnz");
+mongoose.connect(process.env.DATABASEURL);
+//mongoose.connect("mongodb://lyndj:horsedata@ds123919.mlab.com:23919/yelpcampnz");
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs"); 
 app.use(express.static(__dirname + "/public"))
